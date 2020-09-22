d4864ac47f961d862e332cfdc036451a7b254c70
Chantal Snazel <clsnazel@gmail.com>
2020-09-22 22:56:57 +0000

    completed testing activity for async and sync implementations

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf-8', (error, data) => {
+    console.log('In readFile\'s callback: it has the data');
+    if(!error) callback(data);
+    else{
+      callback(undefined);
+    }
+  });
+
+}
+
+// const bombay = breedDetailsFromFile('Bombay', (data) => {
+//   console.log('return value:', data);
+// });
+
+module.exports = breedDetailsFromFile;
